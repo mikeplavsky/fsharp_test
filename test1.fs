d15033374379a0a03e8d58daf1e9ee795fe2845a
Mike Plavsky <Mike.Plavsky@quest.com>
2012-09-14 14:16:45 +0000

    more

--- test1.fs
+++ test1.fs
@@ -1,1 +1,11 @@
-printfn "Hello, World!"
+open "System"
+
+let main (args: string[]) = 
+  
+  if args.Length <> 2 then
+    failwith "Errror: expected arguments <greeting> and <thing>"
+  
+  let greeting, thing = args.[0], args.[1]
+  let timeOfDay = DateTime.Now.ToString "hh:mm tt"
+
+  printfn "%s, %s, at %s" greeting thing timeOfDay 
